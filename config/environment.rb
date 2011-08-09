77d6b35208a16b501247bc277156f0018f89e9ab
Kazuya Takeshima <mail@mitukiii.jp>
2011-08-09 16:58:08 +0900

    Add TINAMI configuration.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 TinamiViewer::Application.initialize!
+
+TINAMI.configure do |config|
+  config.api_key = '*** api key ***'
+end

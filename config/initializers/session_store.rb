4d585b2a5df940c6b14ad2fe6a7f70ee5d06baeb
Kazuya Takeshima <mail@mitukiii.jp>
2011-11-24 00:30:47 +0900

    Change that session configuration to expire to 1 week after.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-TinamiViewer::Application.config.session_store :cookie_store, key: '_tinami-viewer_session'
+TinamiViewer::Application.config.session_store :cookie_store, {
+  key: '_tinami-viewer_session',
+  expire_after: 1.week
+}
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
